2b57052ac4c299bfead058db94deb18a267c7825
Mikhail Mednikh <mmednikh@gmail.com>
2020-07-31 13:27:41 -0400

    new files

--- cake/src/components/ProductList.js
+++ cake/src/components/ProductList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import Product from "./Product";
+import Product from "./Product";
 import Title from "./Title";
 import { storeProducts } from "../data";
 import { ProductConsumer } from "../context";
